4ac146de9d07cd9386f8c35a5561eb80803f345c
Jarrett Smolarkiewicz <68030206+jarrettsmo@users.noreply.github.com>
2022-12-10 17:09:30 -0600

    Finalize app design

--- Files_JavaScript/db.js
+++ Files_JavaScript/db.js
@@ -1,9 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-analytics.js";
-//import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-//import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
-//import { initializeApp } from '/firebase/app';
-//import { getAnalytics } from "/firebase/analytics";
 import {
   getFirestore,
   collection,
@@ -15,15 +11,10 @@ import {
   doc,
   updateDoc
 } 
-//from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
 from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js";
 
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Shortstuff App Firebase Configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBUHRkerYlu07ZaEQfVLjX9t7XWljPIgjM",
   authDomain: "shortstuff-14e13.firebaseapp.com",
@@ -46,14 +37,14 @@ async function getShortcuts(db) {
   return ShortcutsList;
 }
 
-//Read Existing Shortcuts
+// Read Existing Shortcuts and Log in Console
 getShortcuts(db).then((docs) => {
   docs.forEach((d) => {
     console.log(d.data());
   });
 });
 
-//Shortcuts App DocumentChange
+// Shortcuts App DocumentChange
 const unsub = onSnapshot(collection(db, "Shortcuts"), (doc) => {
   //   console.log(doc.docChanges());
   doc.docChanges().forEach((change) => {
@@ -62,10 +53,6 @@ const unsub = onSnapshot(collection(db, "Shortcuts"), (doc) => {
       //Reads the document in Shortcuts collection
       renderShortcut(change.doc.data(), change.doc.id);
     }
-    if (change.type === "modified") {
-      //Updates the document in Shortcuts collection
-      renderShortcut(change.doc.data());
-    }
     if (change.type === "removed") {
       //Deletes the document in Shortcuts collection
       removeShortcut(change.doc.id);
@@ -73,7 +60,7 @@ const unsub = onSnapshot(collection(db, "Shortcuts"), (doc) => {
   });
 });
 
-//Create/Add New Shortcut
+// Create/Add New Shortcut
 const formAdd = document.querySelector("#formAddNewShortcut");
 formAdd.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -86,70 +73,7 @@ formAdd.addEventListener("submit", (event) => {
   formAdd.Link.value = "";
 });
 
-//Update/Edit Shortcut
-const ShortcutEditForm = document.querySelector(".Shortcuts");
-ShortcutEditForm.addEventListener("click", (event) => {
-
-  if (event.target.textContent === "edit") {
-    const id = event.target.getAttribute("data-id");
-    const idSelected = document.querySelector("#targetShortcut");
-
-    async function getShortcutTitle(id) {
-      const docRef = doc(db, "Shortcuts", id);
-      const docSnap = await getDoc(docRef);
-      const stuff = docSnap.data();
-      idSelected.innerHTML = stuff.Title;
-    }
-    getShortcutTitle(id);
-  }
-});
-
-const formEdit = document.querySelector("#formEditNewShortcut");
-formEdit.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  // async function editTheShortcut() {
-  //   db.collection(".Shortcuts").doc(id).update({
-  //     Title: formEdit.TitleEdit.value,
-  //     Link: formEdit.LinkEdit.value,
-  //   }).catch((error) => console.log(error));
-  //   formEdit.TitleEdit.value = "";
-  //   formEdit.LinkEdit.value = "";
-  // }
-  // editTheShortcut();
-
-//   async function getShortcutTitle(id) {
-//     const docRef = doc(db, "Shortcuts", id);
-//     const docSnap = await getDoc(docRef);
-//     const stuff = docSnap.data();
-//     idSelected.innerHTML = stuff.Title;
-//   }
-//   editTheShortcut(id);
-// }
-
-  // async function getShortcutTitle() {
-  //   const docRef = doc(db, "Shortcuts", id);
-  //   const docSnap = await getDoc(docRef);
-  //   const stuff = docSnap.data();
-  //   console.log(stuff.Title);
-  // }
-  // getShortcutTitle();
-
-
-
-  //  const ShortcutsContainers = document.querySelector(".Shortcuts");
-  //  ShortcutsContainers.addEventListener("click", (event) => {
-    // console.log(event);
-
-  // getDoc(collection(db, "Shortcuts"), {
-  //   Title: formEdit.Title.value,
-  //   Link: formEdit.Link.value,
-  // }).catch((error) => console.log(error));
-  // formEdit.Title.value = "";
-  // formEdit.Link.value = "";
-});
-
-//Delete Shortcut
+// Delete Shortcut
 const ShortcutContainer = document.querySelector(".Shortcuts");
 ShortcutContainer.addEventListener("click", (event) => {
   // console.log(event);
@@ -158,21 +82,3 @@ ShortcutContainer.addEventListener("click", (event) => {
   deleteDoc(doc(db, "Shortcuts", id));
   }
 });
-
-// *** TEST FUNCTION ***
-// const ShortcutContainer = document.querySelector(".Shortcuts");
-// ShortcutContainer.addEventListener("click", (event) => {
-  // console.log(event);
-  // if (event.target.tagName === "I") {
-  // if (event.target.textContent === "delete") {
-  //   const id = event.target.getAttribute("data-id");
-  // deleteDoc(doc(db, "Shortcuts", id));
-  // }
-  // async function getShortcutTitle() {
-  //   const docRef = doc(db, "Shortcuts", id);
-  //   const docSnap = await getDoc(docRef);
-  //   const stuff = docSnap.data();
-  //   alert(stuff.Title);
-  // }
-  // getShortcutTitle();
-// });
--- Files_JavaScript/ui.js
+++ Files_JavaScript/ui.js
@@ -1,21 +1,20 @@
+// Selector of All Elements with "Shortcuts" Class
 const Shortcuts = document.querySelector(".Shortcuts");
 
+// Document Listeners After All DOM Content Loaded
 document.addEventListener("DOMContentLoaded", function () {
   //Nav Menu
   const menus = document.querySelectorAll(".side-menu");
-  //M.Sidenav.init(menus, { edge: "right" });
-  //const dropdowns = document.querySelectorAll(".dropdown-menu");
-  //M.Sidenav.init(menus, { edge: "right" });
   // Add Shortcuts
   const forms = document.querySelectorAll(".side-form");
-  //M.Sidenav.init(forms, { edge: "left" });
 });
 
+// Render Shortcuts Function
 function renderShortcut(data, id) {
   const html = `
   <div class="Shortcuts flex-container" data-id="${id}">
     <div class="customCardPanel valign-wrapper">
-      <div class="col s9 left-align">
+      <div class="col s10 left-align">
         <a
           class=""
           href="${data.Link}"
@@ -23,71 +22,32 @@ function renderShortcut(data, id) {
           >${data.Title}
         </a>
       </div>
-      <div class="col s3 right-align">
-        <button class="shortcutEdit shortcut-edit btn-floating btn-small amber">
-          <i class="material-icons" data-id="${id}">edit</i>
-        </button>
-        <button class="shortcutEdit shortcut-delete btn-floating btn-small red">
+      <div class="col s2 right-align">
+        <button class="shortcut-delete btn-floating btn-small red">
           <i class="material-icons" data-id="${id}">delete</i>
         </button>
       </div>
     </div>
   </div>
   `;
-
   Shortcuts.innerHTML += html;
 }
 
-/*
-const Templates = document.querySelector(".Templates");
-
-document.addEventListener("DOMContentLoaded", function () {
-  //Nav Menu
-  const menus = document.querySelectorAll(".side-menu");
-  //M.Sidenav.init(menus, { edge: "right" });
-  //const dropdowns = document.querySelectorAll(".dropdown-menu");
-  //M.Sidenav.init(menus, { edge: "right" });
-  // Add Shortcuts
-  const forms = document.querySelectorAll(".side-form");
-  //M.Sidenav.init(forms, { edge: "left" });
-});
-
-function renderTemplate(data, id) {
-  const htmls = `
-  <div class="Templates" data-id="${id}">
-    <h4>${data.Name}</h4>
-    <button>Save Template</button>
-    <textarea class="primaryFocus"></textarea>
-  </div>
-  `;
-
-  Shortcuts.innerHTML += htmls;
-}
-*/
-
-//remove shortcut from DOM
+// Remove Shortcut from DOM
 const removeShortcut = (id) => {
   const Shortcuts = document.querySelector(`.Shortcuts[data-id="${id}"]`);
   // console.log(Shortcut);
   Shortcuts.remove();
 };
 
-//Toggle Form to Add New Shortcut
+// Toggle Form to Add New Shortcut
 $("#addBtn").click(function(){
   $("#addNewShortcut").toggle();
-  $("#editingShortcut, button.shortcutEdit").hide();
+  $(".shortcut-delete").hide();
 });
 
-//Toggle Shortcut Edit/Delete options
-$("#editBtn").click(function(){
-  $("button.shortcutEdit").toggle();
+// Toggle Shortcut Edit/Delete options
+$("#removeBtn").click(function(){
+  $(".shortcut-delete").toggle();
   $("#addNewShortcut").hide();
 });
-
-//Toggle Form to Edit Existing Shortcut
-$( document.body ).click(function() {
-  $("button.shortcutEdit").click(function(){
-    $("#editingShortcut").toggle();
-    $("#addNewShortcut").hide();
-  }); 
-}); 
